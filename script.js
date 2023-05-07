c32cb8481504da0e3300bc5abecdd62474622132
Karelyn Rojas <knrojas@ucdavis.edu>
2023-05-06 22:12:14 -0700

    make it easier to reuse code

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(function () {
-  var todaysWeather = $("#todays-forecast-info");
   var todayTitle = $(".today-title");
   var searchButton = $("#fetch-button");
   var defaultCity = "Davis";
@@ -29,6 +28,10 @@ $(function () {
   var icon5 = null;
   var todaysDate = dayjs();
 
+  const convertToFahrenheit = (temp) => {
+    return ((temp * 9) / 5 - 459.67).toFixed(2);
+  };
+
   // set the day of the week
   todayTitle.text(`
     ${defaultCity} (${todaysDate.format("M/D/YYYY")})
@@ -37,7 +40,7 @@ $(function () {
   // get 5 days from today and set the text
   for (let i = 1; i < 6; i++) {
     const daily = todaysDate.add(i, "day");
-    $(`#day-${i}`).text(`
+    $(`#day-${i}-title`).text(`
     ${daily.format("M/D/YYYY")}
     `);
   }
@@ -47,84 +50,32 @@ $(function () {
     let userInput = $(`#search-input`).val();
 
     if (userInput.length !== 0) {
-      getAndSetWeatherInfo();
+      await getWeatherInfo(userInput);
+      setWeatherInfo(userInput);
     }
   });
 
-  const getAndSetWeatherInfo = async () => {
+  const setWeatherText = async (day, temp, wind, humidity, icon) => {
+    $(`#day-${day}-weather-icon`).attr(
+      "src",
+      `http://openweathermap.org/img/wn/${icon}.png`
+    );
+    $(`#day-${day}-temp`).text(`Temp: ${temp} °F`);
+    $(`#day-${day}-wind`).text(`Wind: ${wind} MPH`);
+    $(`#day-${day}-humidity`).text(`Humidity: ${humidity} %`);
+  };
+
+  const setWeatherInfo = async (userInput) => {
     if (temp && wind && humidity) {
-      $("#today-weather-icon").attr(
-        "src",
-        `http://openweathermap.org/img/wn/${icon}.png`
-      );
-      todaysWeather.text(
-        `
-          ${todaysDate.format("M/D/YYYY")}
-          Temp: ${temp}
-            Wind: ${wind}
-            Humidity: ${humidity}
-          `
-      );
-      $("#day-1-weather-icon").attr(
-        "src",
-        `http://openweathermap.org/img/wn/${icon1}.png`
-      );
-      $("#day-1").text(
-        `
-          ${todaysDate.add(1, "day").format("M/D/YYYY")}
-          Temp: ${temp1}
-            Wind: ${wind1}
-            Humidity: ${humidity1}
-          `
-      );
-      $("#day-2-weather-icon").attr(
-        "src",
-        `http://openweathermap.org/img/wn/${icon2}.png`
-      );
-      $("#day-2").text(
-        `
-           ${todaysDate.add(2, "day").format("M/D/YYYY")}
-          Temp: ${temp2}
-            Wind: ${wind2}
-            Humidity: ${humidity2}
-          `
-      );
-      $("#day-3-weather-icon").attr(
-        "src",
-        `http://openweathermap.org/img/wn/${icon3}.png`
-      );
-      $("#day-3").text(
-        `
-          ${todaysDate.add(3, "day").format("M/D/YYYY")}
-          Temp: ${temp3}
-            Wind: ${wind3}
-            Humidity: ${humidity3}
-          `
-      );
-      $("#day-4-weather-icon").attr(
-        "src",
-        `http://openweathermap.org/img/wn/${icon4}.png`
-      );
-      $("#day-4").text(
-        `
-         ${todaysDate.add(4, "day").format("M/D/YYYY")}
-          Temp: ${temp4}
-            Wind: ${wind4}
-            Humidity: ${humidity4}
-          `
-      );
-      $("#day-5-weather-icon").attr(
-        "src",
-        `http://openweathermap.org/img/wn/${icon5}.png`
-      );
-      $("#day-5").text(
-        `
-          ${todaysDate.add(5, "day").format("M/D/YYYY")}
-          Temp: ${temp5}
-            Wind: ${wind5}
-            Humidity: ${humidity5}
-          `
-      );
+      todayTitle.text(`
+        ${userInput} (${todaysDate.format("M/D/YYYY")})
+      `);
+      setWeatherText(0, temp, wind, humidity, icon);
+      setWeatherText(1, temp1, wind1, humidity1, icon1);
+      setWeatherText(2, temp2, wind2, humidity2, icon2);
+      setWeatherText(3, temp3, wind3, humidity3, icon3);
+      setWeatherText(4, temp4, wind4, humidity4, icon4);
+      setWeatherText(5, temp5, wind5, humidity5, icon5);
     }
   };
 
@@ -142,32 +93,32 @@ $(function () {
       .then((res) => {
         console.log(res);
 
-        temp = res.list[0].main.temp;
+        temp = convertToFahrenheit(res.list[0].main.temp);
         humidity = res.list[0].main.humidity;
         wind = res.list[0].wind.speed;
         icon = res.list[0].weather[0].icon;
 
-        temp1 = res.list[1].main.temp;
+        temp1 = convertToFahrenheit(res.list[1].main.temp);
         humidity1 = res.list[1].main.humidity;
         wind1 = res.list[1].wind.speed;
         icon1 = res.list[1].weather[0].icon;
 
-        temp2 = res.list[2].main.temp;
+        temp2 = convertToFahrenheit(res.list[2].main.temp);
         humidity2 = res.list[2].main.humidity;
         wind2 = res.list[2].wind.speed;
         icon2 = res.list[2].weather[0].icon;
 
-        temp3 = res.list[3].main.temp;
+        temp3 = convertToFahrenheit(res.list[3].main.temp);
         humidity3 = res.list[3].main.humidity;
         wind3 = res.list[3].wind.speed;
         icon3 = res.list[3].weather[0].icon;
 
-        temp4 = res.list[4].main.temp;
+        temp4 = convertToFahrenheit(res.list[4].main.temp);
         humidity4 = res.list[4].main.humidity;
         wind4 = res.list[4].wind.speed;
         icon4 = res.list[4].weather[0].icon;
 
-        temp5 = res.list[5].main.temp;
+        temp5 = convertToFahrenheit(res.list[5].main.temp);
         humidity5 = res.list[5].main.humidity;
         wind5 = res.list[5].wind.speed;
         icon5 = res.list[5].weather[0].icon;
